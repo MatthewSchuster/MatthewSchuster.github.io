cc8894a6b972de9cd3f000b1620ff1d1bcca4b77
MatthewSchuster <goaliegamer8562@gmail.com>
2026-09-24 14:35:27 -0400

    Add files via upload

---
+++ projects/project01-02/project01-02.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: 
+    Date:   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+var service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+let service1Speed = "50Mbps";
+let service2Speed = "100Mbps";
+let service3Speed = "500Mbps";
+let service4Speed = "1Gig";
